77fb1b8157bc5e30bd8cf21d954f9170a9528ae8
mllx87 <springwen87@gmail.com>
2024-07-05 13:57:49 +0800

    add image module

--- gulp/build.js
+++ gulp/build.js
@@ -2,10 +2,33 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify'); // 获取 uglify 模块（用于压缩 JS）
 var babel = require('gulp-babel'); // 将ES6编译成ES5
 
-gulp.task('js', function () {
-    return gulp.src('./src/docx-module/js/**/*.js')
+gulp.task('docx', function () {
+    return gulp.src('./src/docx-module/**/*.js')
         .pipe(babel())              //语法编译 
-   //     .pipe(uglify())             //代码压缩
+        //     .pipe(uglify())             //代码压缩
+        .pipe(gulp.dest('./lib/docx-module'));
+
+})
+
+gulp.task('image', function () {
+    return gulp.src('./src/image-module/open/*.js')
+        .pipe(babel())              //语法编译 
+        .pipe(gulp.dest('./lib/image-module'));
+
+})
+
+gulp.task('index', function () {
+    return gulp.src(['./src/index.js'])
+        .pipe(babel())              //语法编译 
         .pipe(gulp.dest('./lib'));
 
 })
+
+gulp.task('common', function () {
+    return gulp.src(['./src/common/*.js'])
+        .pipe(babel())              //语法编译 
+        .pipe(gulp.dest('./lib/common'));
+
+})
+
+gulp.task('build', gulp.series(gulp.parallel('docx', 'image', 'index', 'common')));
--- tests/index.js
+++ tests/index.js
@@ -1,14 +1,21 @@
-const Puredocx = require("../index.js");
+const Puredocx = require("../lib/index.js");
 
 
-var puredocx = new Puredocx()
+var puredocx = new Puredocx({
+    "test": "kkk",
+    "imageOptions": {
+        imageRootPath: "",
+        getSize() { return [350, 300] }
+    }
+})
 
 
 var substitutions = {
     "title": "hello",
     "summary": "New World",
     "staff": 30,
-    "company": "mllx87"
+    "company": "mllx87",
+    "image": "99e36.jpeg"
 };
 
 
